7058c3abc070d4b84c996bb5627093781e4ea00c
AnaMariaVladPichiu <amvlad2@gmail.com>
2024-10-09 17:09:54 +0200

    clase_07.10.24_1js

---
+++ HolaMundo2-main/js/ejercicio4/jsejercicio4.js
@@ -0,0 +1,7 @@
+const correctuser = "asiestabien"
+const correctpassword = "tambienasiestabien"
+
+let user = prompt ("Introducir ususario");
+let password = prompt("Introducir contraseña");
+
+if ()
