7cfc62abd6d054e74d494a971702c221d5ddd97e
AnaMariaVladPichiu <amvlad2@gmail.com>
2024-10-09 20:49:34 +0200

    Cambios realizados en los ejercicios de js

--- HolaMundo2-main/JAVASCRIPT/ejercicio4/jsejercicio4.js
+++ HolaMundo2-main/JAVASCRIPT/ejercicio4/jsejercicio4.js
@@ -1,7 +1,23 @@
-const correctuser = "asiestabien"
-const correctpassword = "tambienasiestabien"
+const correctuser = "asiestabien";
+const correctpassword = "tambienasiestabien";
 
-let user = prompt ("Introducir ususario");
-let password = prompt("Introducir contraseña");
+let intentos = 3;
+let user;
+let password;
+ do{
+    user=prompt("Introducir usuario");
+    password=prompt("Introducir contraseña");
+    intentos--;
+    if (user != correctuser || password != correctpassword ) {
+        alert ("Lo que has liado pollitooo! . Acceso denegado");
+    }
 
-if ()
+
+}while ((user != correctuser || password != correctpassword)  && intentos>0);     
+
+
+
+    
+if (user == correctuser && password == correctpassword) {
+    alert("!Lo has conseguido!");
+}  
