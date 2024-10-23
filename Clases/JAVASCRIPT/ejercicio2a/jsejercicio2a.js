9b55013ca616bfb6176b9eea26bdd347ede8603f
MarcoApunto <ferreiras.marcoa@gmail.com>
2024-10-23 17:42:39 +0200

    Actualizado 17:42

---
+++ Clases/JAVASCRIPT/ejercicio2a/jsejercicio2a.js
@@ -0,0 +1,10 @@
+let frase=prompt("Introiducir una frase");
+let letra=prompt("Introducir una letra");//o
+let contador = 0
+//Hola     i=0 ->H, i=1->o
+for(let i=0; i<frase.length; i++) {
+    if (frase[i]==letra){
+        contador++
+    }
+}
+alert("La letra "+ letra + " aparece "+ contador + " veces en la frase.");
